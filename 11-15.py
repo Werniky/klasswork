26f05ef85aa4c7a0b9d433fba70d079715803f96
Werniky <163991857+Werniky@users.noreply.github.com>
2024-04-18 16:05:24 +0300

    Add files via upload

---
+++ 11-15.py
@@ -0,0 +1,56 @@
+#   №11
+#Left_side = int(input("Введите длину левой стороны: "))
+#Right_side = int(input("Введите длину правой стороны: "))
+#Bottom_side = int(input("Введите длину нижней стороны: "))
+#if Left_side == Right_side == Bottom_side:
+#    print ("равносторонний треугольник")
+#elif Left_side == Right_side:
+#    print ("равнобедренный треугольник")
+#else:
+#    print ("разносторонний треугольник")
+
+
+#   №12
+#Fir_num = int(input("Введите 1 число:"))
+#Sec_num = int(input("Введите 2 число: "))
+#Thi_num = int(input("Введите 3 число: "))
+#Res = int((Fir_num+Sec_num+Thi_num) / 3)
+#print (Res)
+
+
+#   №13
+#Months = ([31,28,31,30,31,30,31,31,30,31,30,31])
+#Month_choose = int(input("Введите месяц: "))
+#Month_choose = Month_choose + 1
+#print (Months[Month_choose])
+
+
+#   №14
+#Weight = int(input("Введите вес: "))
+#if Weight <= 30:
+#    print ("Лёгкий вес")
+#elif Weight <= 64:
+#    print ("Первый полусредний вес")
+#elif Weight <= 69:
+#    print ("Полусредний вес")
+#else:
+#    print ("Недопущен")
+
+
+#   №15
+#FNum = int(input("Первое число: "))
+#Symbol = str(input("Символ: "))
+#SNum = int(input("Второе число: "))
+#if Symbol == "+":
+#    print (FNum + SNum)
+#elif Symbol == "-":
+#    print (FNum - SNum)
+#elif Symbol == "*":
+#    print (FNum * SNum)
+#elif Symbol == "/":
+#    if SNum != 0:
+#        print (FNum / SNum)
+#    else:
+#        print ("На ноль делить нельзя!")
+#else:
+#    print ("Неверная операция")
