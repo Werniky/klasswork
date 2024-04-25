834977782c2d1d2c81783a58fb98e4e0d04ccb56
Werniky <163991857+Werniky@users.noreply.github.com>
2024-04-25 19:34:32 +0300

    Add files via upload

---
+++ Temp_2.py
@@ -0,0 +1,50 @@
+##6
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#if a == b == c:
+#    print ("Равносторонний")
+#elif a == b:
+#    print ("Равнобедренный")
+#elif a != b != c:
+#    print ("Разносторонний")
+
+
+##7
+#a = int(input())
+#b = int(input())
+
+
+##8
+#a = int(input())
+#if a == 1 or a == 2 or a == 3:
+#    print ("Зима")
+#elif a == 4 or a == 5 or a == 6:
+#    print ("Весна")
+#elif a == 7 or a == 8 or a == 9:
+#    print ("Лето")
+#elif a == 10 or a == 11 or a == 12:
+#    print ("Осень")
+#else:
+#    print ("Ну, егор")
+
+
+##9
+#a = int(input())
+#if a <= -6:
+#    print ("Зимний набор")
+#elif a >= -5 and a <= 15:
+#    print ("Демисезон")
+#elif a >= 16:
+#    print ("Летний набор")
+
+
+##10
+#print ("Длина прямоугольника 30 (м) от нуля")
+#print ("Высота прямоугольника 20 (м) от нуля")
+#x = int(input())
+#y = int(input())
+#if x <= 30 and y <= 20:
+#    print ("Мы внутри")
+#else:
+#    print ("Мы за МКАДом!!!")
